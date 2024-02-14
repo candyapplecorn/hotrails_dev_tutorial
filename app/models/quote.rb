e5562bfd06dad86a19e3a899092b0bd0cd73b8ec
Joseph Burger <joseph@airkit.com>
2024-02-14 12:53:33 -0600

    feat: added scope to quotes broadcast so that only users in same company receive quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,7 +5,7 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  broadcasts_to ->(quote) { "quotes"}, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
